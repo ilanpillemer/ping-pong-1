57689f27745d24f5cd2320dce94a0edf27c1d0a4
Ilan Pillemer <ilan.pillemer@gmail.com>
2014-04-21 21:07:14 +0200

    remove the last bit of code

--- pingpong/src/main/java/com/ilanpillemer/pingpong/PingPongRight.java
+++ pingpong/src/main/java/com/ilanpillemer/pingpong/PingPongRight.java
@@ -1,6 +1,1 @@
 package com.ilanpillemer.pingpong;
-// Import the necessary Java synchronization and scheduling classes.
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Condition;
